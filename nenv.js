cafb72c9f09e60bc3b8e685e14b465a4686beb21
abr-ya <gamer05@list.ru>
2023-06-14 08:17:31 +0300

    fix: Fix env-handler for Netlify

--- nenv.js
+++ nenv.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
 // здесь необходимо перечислить ключи из файла ENV, а точнее - из ENV netlify
-fs.writeFileSync("./.env", `API_URL=${process.env.API_URL}\n`);
-fs.writeFileSync("./.env", `MAP_KEY=${process.env.MAP_KEY}\n`);
+const ENV = `
+  API_URL=${process.env.API_URL}\n
+  MAP_KEY=${process.env.MAP_KEY}\n
+`;
+console.log(ENV);
+
+fs.writeFileSync("./.env", ENV);
